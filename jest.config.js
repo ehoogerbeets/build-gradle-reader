2772ecbce33df348035a819cf45f9c472462d282
Edwin Hoogerbeets <ehoogerbeets@box.com>
2025-11-05 16:07:41 -0800

    Rename the js tests to ts

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,22 @@
 export default {
     testEnvironment: 'node',
-    transform: {},
+    transform: {
+        '^.+\\.ts$': ['@swc/jest', {
+            jsc: {
+                target: 'es2015',
+                parser: {
+                    syntax: 'typescript',
+                },
+            },
+            module: {
+                type: 'es6',
+            },
+        }],
+    },
     moduleNameMapper: {
         '^(\\.{1,2}/.*)\\.js$': '$1',
     },
-    testMatch: ['**/test/**/*.test.js'],
+    testMatch: ['**/test/**/*.test.ts'],
+    extensionsToTreatAsEsm: ['.ts'],
 };
 
